6efd88bcc26344c76261d25db96a45e7006b4b5c
d.kanellidis@msensis.com <d.kanellidis@msensis.com>
2024-07-22 13:51:05 +0300

    Initial Commit

---
+++ Bookeeping/BookeepingApp.swift
@@ -0,0 +1,20 @@
+//
+//  BookeepingApp.swift
+//  Bookeeping
+//
+//  Created by MSENSIS on 22/7/24.
+//
+
+import SwiftUI
+
+@main
+struct BookeepingApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
